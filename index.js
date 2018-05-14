bfff1906eae4f9bc0d25ab8f17c97e3a6140b546
Braxton Reid <reid74@purdue.edu>
2018-05-14 16:22:29 -0400

    express fear

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const button = document.querySelector("button.greeting");
 
 function changeText() {
-  alert("yeah");
+  const paragraph = document.querySelector("p.greeting");
+  paragraph.textContent = "I am feared";
 }
 
 button.addEventListener("click", changeText);
