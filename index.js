f4b91345d58e07be1cbd0b23024235365c44611c
Braxton Reid <reid74@purdue.edu>
2018-05-15 10:14:07 -0400

    add functionality and improve aesthetics

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-const button = document.querySelector("button.greeting");
+const headerButton = document.querySelector("#changeHeader");
+const header1 = document.querySelector("#first");
+const header2 = document.querySelector("#second");
+const input = document.querySelector("#headerInput");
+const submitButton = document.querySelector("#submit");
 
-function changeText() {
+console.log(input);
+
+function changeText(ev) {
   const paragraph = document.querySelector("p.greeting");
-  paragraph.textContent = "I am feared";
+  if (ev.target.textContent === "Fear Me") {
+    ev.target.textContent = "Love Me";
+    paragraph.textContent = "I am feared";
+  } else if (ev.target.textContent === "Love Me") {
+    ev.target.textContent = "Fear Me";
+    paragraph.textContent = "I am loved";
+  } 
 }
 
-button.addEventListener("click", changeText);
+function changeHeader() {
+  const headerText = header2.textContent;
+  if (headerText === "User Directory") {
+    header2.textContent = "TEST";
+  } else {
+    header2.textContent = "User Directory";
+  }
+}
+
+function setHeader(ev) {
+  header1.textContent = input.value;
+  input.value = "";
+}
+
+submitButton.addEventListener("click", setHeader);
+
+headerButton.addEventListener("click", changeHeader);
